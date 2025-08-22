22c71a384f145a079fce1ab2e8c8d49841d46940
JeanTrey <jeanmarconascimentodarocha@gmail.com>
2025-08-22 11:04:08 -0300

    arrumado cards

--- js/cerebro.js
+++ js/cerebro.js
@@ -1,42 +1,10 @@
-const track = document.getElementById('carrosselTrack');
-const imagens = track.querySelectorAll('img');
-const total = imagens.length;
-let slideWidth = imagens[0].offsetWidth;
-window.addEventListener('resize', () => {
-  slideWidth = imagens[0].offsetWidth;
-});
-
-
+const imagens = document.querySelectorAll('.cerebro-carrossel img');
 let index = 0;
 
-
-for (let i = 0; i < total; i++) {
-  const clone = imagens[i].cloneNode(true);
-  track.appendChild(clone);
+function trocarImagem() {
+  imagens[index].classList.remove('active');
+  index = (index + 1) % imagens.length;
+  imagens[index].classList.add('active');
 }
 
-function moverSlide() {
-  slideWidth = imagens[0].offsetWidth; // garante atualização
-  index++;
-  track.style.transition = 'transform 0.7s ease-in-out';
-  track.style.transform = `translateX(${-index * slideWidth}px)`;
-
-
-  if (index >= total) {
-    setTimeout(() => {
-      track.style.transition = 'none';
-      track.style.transform = 'translateX(0px)';
-      index = 0;
-  
-      // força reflow para garantir o reset
-      void track.offsetWidth;
-  
-      // reativa a transição para o próximo movimento
-      setTimeout(() => {
-        track.style.transition = 'transform 0.7s ease-in-out';
-      }, 20);
-    }, 700);
-  }
-}  
-
-setInterval(moverSlide, 5000);
+setInterval(trocarImagem, 6000); 
