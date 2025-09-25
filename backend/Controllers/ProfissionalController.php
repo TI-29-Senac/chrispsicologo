435431eb2ea2bb8aa569256dd34f9b2af78c6c04
llucasgm <lucas.gomesmm19@gmail.com>
2025-09-25 09:54:34 -0300

    feat: ProfissionalController

---
+++ backend/Controllers/ProfissionalController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Psico\Controllers;
+
+use App\Psico\Models\Profissional;
+use App\Psico\Database\Database;
+
+class ProfissionalController {
+    public $profissional;   
+    public $db;
+    public function __construct(){
+        $this->db = Database::getInstance();
+        $this->profissional = new Profissional($this->db);
+
+    }
+    // Index
+    public function index(){
+        $resultado = $this->profissional->buscarServico();
+        return  $resultado;
+    }
+
+    // Registrar
+
+
+    // Login
+
+
+    // Atualizar
+
+
+    // Deletar
+
+
+    // Chamada de API
+
+
+
+}
