a020201cd7d4f1e4b8d9991ca2bba558e34a4f3c
llucasgm <lucas.gomesmm19@gmail.com>
2025-10-31 09:10:57 -0300

    feat: Sla

--- backend/Rotas/Rotas.php
+++ backend/Rotas/Rotas.php
@@ -23,7 +23,7 @@ class Rotas {
                 "/agendamentos/editar/{id}" => "AgendamentoController@viewEditarAgendamentos",
                 "/agendamentos/excluir/{id}" => "AgendamentoController@viewExcluirAgendamentos",
                 "/api/cliente/meus-agendamentos" => "AgendamentoController@buscarAgendamentosPorUsuarioApi",
-                "/agendamentos/detalhe-pagamento/{id}" => "AgendamentoController@getDetalhesPagamento", // Rota p/ pág. pagamento
+                "/agendamentos/detalhe-pagamento/{id}" => "AgendamentoController@getDetalhesPagamento", 
 
                 // --- AVALIAÇÕES (Admin & API Pública) ---
                 "/avaliacoes" => "AvaliacaoController@buscarPorProfissional", // API pública
